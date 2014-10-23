59d37d84fd6b52e628398e4272a7a7992f88b4a6
Miklos Szeredi <mszeredi@suse.cz>
2014-10-24 00:14:39 +0200

    BACKPORT: fs: limit filesystem stacking depth
    Add a simple read-only counter to super_block that indicates how deep this
    is in the stack of filesystems.  Previously ecryptfs was the only stackable
    filesystem and it explicitly disallowed multiple layers of itself.
    
    Overlayfs, however, can be stacked recursively and also may be stacked
    on top of ecryptfs or vice versa.
    
    To limit the kernel stack usage we must limit the depth of the
    filesystem stack.  Initially the limit is set to 2.
    
    Signed-off-by: Miklos Szeredi <mszeredi@suse.cz>
    
    (cherry picked from commit 69c433ed2ecd2d3264efd7afec4439524b319121)
    
    Bug: 32761463
    Change-Id: I69b2fba2112db2ece09a1bf61a44f8fc4db00820

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -248,6 +248,12 @@ struct iattr {
  */
 #include <linux/quota.h>
 
+/*
+ * Maximum number of layers of fs stack.  Needs to be limited to
+ * prevent kernel stack overflow
+ */
+#define FILESYSTEM_MAX_STACK_DEPTH 2
+
 /** 
  * enum positive_aop_returns - aop return codes with specific semantics
  *
@@ -1319,7 +1325,12 @@ struct super_block {
 	/* Being remounted read-only */
 	int s_readonly_remount;
 
-#define FLAG_ASYNC_FSYNC	0x1
+	/*
+	 * Indicates how deep in a filesystem stack this SB is
+	 */
+	int s_stack_depth;
+
+#define FLAG_ASYNC_FSYNC       0x1
 	unsigned int fsync_flags;
 };
 
